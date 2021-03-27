d74eed0a1101eb5c69a14039b932e16bdbc567b1
Cara Wong <caraalyssa.wong@gmail.com>
2021-03-26 23:10:55 -0700

    add armmagnitudes to persona view state type

--- lib/persona.view.ts
+++ lib/persona.view.ts
@@ -24,6 +24,20 @@ export type PersonaViewState<TPosType = number> = {
   debugName?: string,
   login?: boolean,
   logout?: boolean,
+  armMagnitudes?: {
+    physical: number,
+    sleep: number,
+    mood: number,
+    cognition: number,
+    leisure: number,
+    relationships: number,
+    spiritual: number,
+    money: number,
+    home: number,
+    selfEsteem: number,
+    independence: number,
+    identity: number
+  }
 };
 
 export namespace PersonaViewState {
